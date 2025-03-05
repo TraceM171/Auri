a65eea11e67c180c0035e76db46db997e3b7d255
auri <auri@auri.com>
2025-03-05 20:21:48 +0100

    generalize app initialization

--- app/src/main/kotlin/com/auri/app/App.kt
+++ app/src/main/kotlin/com/auri/app/App.kt
@@ -1,6 +1,8 @@
 package com.auri.app
 
 import arrow.core.getOrElse
+import arrow.core.raise.either
+import arrow.core.raise.withError
 import co.touchlab.kermit.Logger
 import co.touchlab.kermit.Severity
 import com.auri.app.collection.CollectionProcessStatus
@@ -19,6 +21,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.transformWhile
 import kotlinx.coroutines.launch
+import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import java.io.File
@@ -26,51 +29,41 @@ import java.io.File
 suspend fun CoroutineScope.launchSampleCollection(
     baseDirectory: File,
     runbook: File,
+    minLogSeverity: Severity,
     pruneCache: Boolean
 ): Flow<CollectionProcessStatus> {
-    val cacheDir = File(baseDirectory, "cache/collection")
-    val samplesDir = File(baseDirectory, "samples")
-    val extensionsDir = File(baseDirectory, "extensions")
-    val auriDB = File(baseDirectory, "auri.db").let(::sqliteConnection)
-
-    Logger.setMinSeverity(Severity.Warn) // TODO
-
-    if (pruneCache) {
-        cacheDir.deleteRecursively()
+    val initContext = init(
+        baseDirectory = baseDirectory,
+        runbook = runbook,
+        minLogSeverity = minLogSeverity,
+        pruneCache = pruneCache
+    ).getOrElse {
+        return when (it) {
+            is InitError.GettingMainConfig -> MutableStateFlow(
+                CollectionProcessStatus.Failed(
+                    what = "Failed to load main segment of runbook",
+                    why = it.cause.message ?: "Unknown error"
+                )
+            )
+        }
     }
 
-    baseDirectory.mkdirs()
-    cacheDir.mkdirs()
-    samplesDir.mkdirs()
-    extensionsDir.mkdirs()
-    newSuspendedTransaction(context = Dispatchers.IO, db = auriDB) {
-        SchemaUtils.create(RawSampleTable, SampleInfoTable)
-    }
-
-    runbook.configByPrefix<MainConf>().getOrElse {
+    val phaseConfig = runbook.configByPrefix<CollectionPhaseConfig>(
+        classLoader = initContext.classLoader,
+        prefix = "collectionPhase"
+    ).getOrElse {
         return MutableStateFlow(
             CollectionProcessStatus.Failed(
-                what = "Failed to load main segment of runbook",
+                what = "Failed to load collection phase segment of runbook",
                 why = it.message ?: "Unknown error"
             )
         )
     }
-    val classLoader = Thread.currentThread().contextClassLoader
-        .chainIfNotNull(extensionsDir) { withExtensions(it) }
-    val phaseConfig =
-        runbook.configByPrefix<CollectionPhaseConfig>(classLoader = classLoader, prefix = "collectionPhase").getOrElse {
-            return MutableStateFlow(
-                CollectionProcessStatus.Failed(
-                    what = "Failed to load collection phase segment of runbook",
-                    why = it.message ?: "Unknown error"
-                )
-            )
-        }
 
     val collectionService = CollectionService(
-        cacheDir = cacheDir,
-        samplesDir = samplesDir,
-        auriDB = auriDB,
+        cacheDir = initContext.cacheDir,
+        samplesDir = initContext.samplesDir,
+        auriDB = initContext.auriDB,
         collectors = phaseConfig.collectors,
         infoProviders = phaseConfig.infoProviders
     )
@@ -89,4 +82,57 @@ suspend fun CoroutineScope.launchSampleCollection(
             is CollectionProcessStatus.Failed -> false
         }
     }
+}
+
+
+private suspend fun init(
+    baseDirectory: File,
+    runbook: File,
+    minLogSeverity: Severity,
+    pruneCache: Boolean
+) = either<InitError, InitContext> {
+    val cacheDir = File(baseDirectory, "cache/collection")
+    val samplesDir = File(baseDirectory, "samples")
+    val extensionsDir = File(baseDirectory, "extensions")
+    val auriDB = File(baseDirectory, "auri.db").let(::sqliteConnection)
+
+    Logger.setMinSeverity(minLogSeverity)
+
+    val mainConfig = withError({ InitError.GettingMainConfig(it) }) {
+        runbook.configByPrefix<MainConf>().bind()
+    }
+    val classLoader = Thread.currentThread().contextClassLoader
+        .chainIfNotNull(extensionsDir) { withExtensions(it) }
+
+    if (pruneCache) {
+        cacheDir.deleteRecursively()
+    }
+
+    baseDirectory.mkdirs()
+    cacheDir.mkdirs()
+    samplesDir.mkdirs()
+    extensionsDir.mkdirs()
+    newSuspendedTransaction(context = Dispatchers.IO, db = auriDB) {
+        SchemaUtils.create(RawSampleTable, SampleInfoTable)
+    }
+
+    InitContext(
+        cacheDir = cacheDir,
+        samplesDir = samplesDir,
+        auriDB = auriDB,
+        mainConfig = mainConfig,
+        classLoader = classLoader
+    )
+}
+
+private data class InitContext(
+    val cacheDir: File,
+    val samplesDir: File,
+    val auriDB: Database,
+    val mainConfig: MainConf,
+    val classLoader: ClassLoader
+)
+
+private sealed interface InitError {
+    data class GettingMainConfig(val cause: Throwable) : InitError
 }
