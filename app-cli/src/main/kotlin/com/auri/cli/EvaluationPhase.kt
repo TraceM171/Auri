c155dfd882832c0d4eddb1ebe8398ebaf3d30a70
auri <auri@auri.com>
2025-05-07 12:06:36 +0200

    fix evaluation ui bug

--- app-cli/src/main/kotlin/com/auri/cli/EvaluationPhase.kt
+++ app-cli/src/main/kotlin/com/auri/cli/EvaluationPhase.kt
@@ -227,7 +227,7 @@ private fun VerticalLayoutBuilder.evaluationStatsTui(evaluationStats: Evaluation
         body {
             evaluationStats.vendorStats.forEach { (vendor, stats) ->
                 val blockRate = stats.blockedSamples / stats.analyzedSamples.toDouble()
-                val blockRateText = if (blockRate.isNaN()) "" else " (%.2f%)".format(blockRate * 100)
+                val blockRateText = if (blockRate.isNaN()) "" else " (%.2f%%)".format(blockRate * 100)
                 row {
                     cell(Text(vendor.name, whitespace = Whitespace.PRE_WRAP))
                     cell(Text("${stats.blockedSamples} of ${stats.analyzedSamples}$blockRateText")) {
